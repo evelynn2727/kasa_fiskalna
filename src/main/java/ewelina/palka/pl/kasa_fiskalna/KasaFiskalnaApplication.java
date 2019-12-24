37ea870adfb9b7a5079bf7239bbb33ef968a3c61
Ewelina Pałka <totalnieniewybaczalne.ep@gmail.com>
2019-12-24 23:31:37 +0100

    Initial commit

---
+++ src/main/java/ewelina/palka/pl/kasa_fiskalna/KasaFiskalnaApplication.java
@@ -0,0 +1,13 @@
+package ewelina.palka.pl.kasa_fiskalna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasaFiskalnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasaFiskalnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ewelina/palka/pl/kasa_fiskalna/KasaFiskalnaApplicationTests.java
@@ -0,0 +1,13 @@
+package ewelina.palka.pl.kasa_fiskalna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasaFiskalnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
